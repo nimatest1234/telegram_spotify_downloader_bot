9faf12c0b34b05e63f80fa0a0726e5984d7c3a08
nimatest1234 <86647835+nimatest1234@users.noreply.github.com>
2021-06-29 11:40:58 +0430

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -14,7 +14,7 @@ spotifyy = spotipy.Spotify(
                                                         client_secret='389614e1ec874f17b8c99511c7baa2f6'))
 genius = lyricsgenius.Genius('biZZReO7F98mji5oz3cE0FiIG73Hh07qoXSIzYSGNN3GBsnY-eUrPAVSdJk_0_de')
 
-token = 'token bot'
+token = '1742165384:AAHsWtWnOn0eOMUPD9QJmqEtMMK4pwK3fJ0'
 
 bot = telepot.Bot(token)
 
